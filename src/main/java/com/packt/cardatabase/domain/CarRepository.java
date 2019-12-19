3b9e4185360f924992d619072be9077b9c4bf3b3
Justin Mulcahy <Justin.Mulcahy@gmail.com>
2019-12-19 17:14:31 +0000

    Security Update
    Security

--- src/main/java/com/packt/cardatabase/domain/CarRepository.java
+++ src/main/java/com/packt/cardatabase/domain/CarRepository.java
@@ -2,10 +2,12 @@ package com.packt.cardatabase.domain;
 
 import com.packt.cardatabase.entity.Car;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 import java.util.Optional;
 
+@RepositoryRestResource
 public interface CarRepository extends CrudRepository <Car, Long> {
 
 
