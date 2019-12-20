35a1da02eddc43ffe24edf890bc387a83aafdc36
Justin Mulcahy <Justin.Mulcahy@gmail.com>
2019-12-20 11:50:53 +0000

    security

--- src/main/java/com/packt/cardatabase/CardatabaseApplication.java
+++ src/main/java/com/packt/cardatabase/CardatabaseApplication.java
@@ -2,8 +2,10 @@ package com.packt.cardatabase;
 
 import com.packt.cardatabase.domain.CarRepository;
 import com.packt.cardatabase.domain.OwnerRepository;
+import com.packt.cardatabase.domain.UserRepository;
 import com.packt.cardatabase.entity.Car;
 import com.packt.cardatabase.entity.Owner;
+import com.packt.cardatabase.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -18,6 +20,9 @@ public class CardatabaseApplication {
 
 	@Autowired
 	private OwnerRepository orepository;
+
+	@Autowired
+	private UserRepository urepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CardatabaseApplication.class, args);
@@ -35,6 +40,9 @@ public class CardatabaseApplication {
 			repository.save(new Car("Ford","Mustang","Red","ADF-1121",2017,59000,"",owner1));
 			repository.save(new Car("Nissan","Leaf","White","SSJ-3002",2014,29000,"",owner2));
 			repository.save(new Car("Toyota","Prius","Solver","KKO-0212",2018,39000,"",owner2));
+
+			urepository.save(new User("user", "$2a$04$1.YhMIgNX/8TkCKGFUONWO1waedKhQ5KrnB30fl0Q01QKqmzLf.Zi", "USER"));
+			urepository.save(new User("admin", "$2a$04$KNLUwOWHVQZVpXyMBNc7JOzbLiBjb9Tk9bP7KNcPI12ICuvzXQQKG", "ADMIN"));
 		};
 	}
 
---
+++ src/main/java/com/packt/cardatabase/domain/UserRepository.java
@@ -0,0 +1,11 @@
+package com.packt.cardatabase.domain;
+
+import com.packt.cardatabase.entity.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    User findByUsername(String username);
+}
